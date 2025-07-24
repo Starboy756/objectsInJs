a35996fd0233f95a11b710c40d2091815d31b2ea
Starboy756 <taseentariq5@gmail.com>
2025-07-24 23:38:07 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+person = {
+    firstName: "Taseen",
+    lastName: "Tariq",
+    isEmployed: false,
+    age: 19,
+    sayHello: () => console.log(`Taseen Says Hello`),
+    sayBye: () => console.log(`Taseen Says Bye`)
+}
+
+person.sayBye();
+person.sayHello();
+console.log(person.age)
