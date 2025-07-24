e4c9f995d867222ece52e8b269389cb7ff35d3a6
Starboy756 <taseentariq5@gmail.com>
2025-07-24 23:51:59 +0500

    added new things

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-person = {
-    firstName: "Taseen",
-    lastName: "Tariq",
-    isEmployed: false,
-    age: 19,
-    sayHello: () => console.log(`Taseen Says Hello`),
-    sayBye: () => console.log(`Taseen Says Bye`)
+person1 = {
+    name: "SpongeBob",
+    favFood: "HamBurger",
+    sayHello: () => {console.log(`${person1.name} Says Hello`)}, //this keyword works only with function expression , if we want to use arrow function we will have to use person1.name
+    eat: () => {console.log(`${person1.name} eats his Favfood which is ${person1.favFood}`)}
 }
-
-person.sayBye();
-person.sayHello();
-console.log(person.age)
+person1.eat();
